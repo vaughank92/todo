a8c9ed71ea5d9f46c0633e5745b9ebba55c2f9e5
Katie Vaughan <vaughank92@gmail.com>
2018-02-20 13:58:12 -0500

    todo model

--- server.js
+++ server.js
@@ -26,10 +26,71 @@ app.use(bodyParser.urlencoded({'extended': 'true'}));
 //parse application/json
 app.use(bodyParser.json());
 //parse application/vnd.api+json as json
-app.use(bodyParse.json({type: 'application/vnd.api+json'}));
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
 //listen (start app with node server.js)
 app.listen(8080);
 
 console.log("App listening at port 8080");
+
+//model
+
+var Todo = mongoose.model('Todo', {
+	text : String
+});
+
+//routes
+
+//api
+//get all todos
+app.get('/api/todos', function(req, res) {
+	//mongoose gets all the todos
+	Todo.find(function(err, todos) {
+		if(err)
+			res.send(err);
+		res.json(todos);
+	});
+});
+
+//create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+	//create todo
+	Todo.create({
+		text : req.body.text,
+		done : false
+	}, function(err, todo) {
+		if(err)
+			res.send(err);
+
+		//get and return all todos
+		Todo.find(function(err, todos) {
+			if(err)
+				res.send(err)
+			res.json(todos);
+		});
+	});
+});
+
+//delete
+app.delete('/api/todos/:todo_id', function(req, res) {
+	Todo.remove({
+		_id : req.params.todo_id
+	}, function(err, todo) {
+		if(err)
+			res.send(err);
+
+		//get and return all todos
+		Todo.find(function(err, todos) {
+			if(err)
+				res.send(err)
+			res.json(todos);
+		});
+	});
+});
+
+
+
+
+
+
